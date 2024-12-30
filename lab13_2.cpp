a105019ab7c63248b8d0f3302dfde009d708058c
Chiraphat-t <chiraphat.ttt@gmail.com>
2024-12-30 14:48:55 +0700

    2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,42 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool m[][M],int s,int x,int y){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            double z= sqrt(pow((i-x),2)+pow((j-y),2));
+            if(z<=s-1){
+                m[i][j]=1;
+            }
+
+        }
+    }
+
+}
+
+
+void showImage(const bool m[][M]){
+
+cout << "------------------------------------------------------------------------\n";
+
+    for(int i=0;i<N;i++){
+    
+        for(int j=-1; j<=M ; j++){
+            if(j==-1||j==M){
+                cout << "|";
+            }
+            else if(m[i][j]==1){
+                cout << "*";
+            }
+            else if(m[i][j]==0){
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+cout << "------------------------------------------------------------------------";  
+cout << endl;  
+}
+
